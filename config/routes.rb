dcc5e90f1dcfc0978cf165f33ffe299071d83d42
lucasprado <lucaspradobr@gmail.com>
2023-11-14 13:33:08 -0300

    create action step 1: get the form

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  get 'tasks', to: 'tasks#index', as: :resturants
-
+  get 'tasks', to: 'tasks#index', as: :tasks
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  get 'tasks/:id', to: 'tasks#show', as: :task
 end
